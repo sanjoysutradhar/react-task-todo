269c05aafb057597e322622dc45b8316a8932c0a
Shanjoy Sutradhar <shanjoy.swe@gmail.com>
2024-12-01 11:18:18 +0600

    basic set of react project with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-blue-500">
+      <h1 className="text-4xl font-bold text-white">
+        Hello, React and Tailwind CSS!
+      </h1>
+    </div>
+  );
+}
